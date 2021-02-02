430a7ee3c79a2231ba34308ba564d6ad03133f4a
pratikshagaur <55282137+pratikshagaur@users.noreply.github.com>
2021-02-02 16:47:09 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,35 @@
+
+//restart game button
+var restart = document.querySelector("#bt");
+
+//grab all the squares
+var sqr = document.querySelectorAll("td");
+//clear all the squares
+function clearsqr()
+{
+	for(var i =0 ;i<sqr.length; i++)
+	 {
+   	  sqr[i].textContent= '';
+	 }
+
+}
+
+restart.addEventListener('click',clearsqr)
+
+function changeMarker()
+{
+    if(this.textContent === '')
+    {
+      this.textContent = 'X';
+      // console.log(marker)
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    }else {
+      this.textContent = '';
+    }
+};
+
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < sqr.length; i++) {
+    sqr[i].addEventListener('click', changeMarker);
+}
